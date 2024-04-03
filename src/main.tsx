f4d08ae08b5003b6cffe78269d5e2b0f6d00644e
michalrozekariane <michal.rozek@arianelabs.com>
2024-04-03 15:31:19 +0200

    feat: unneeded packages uninstalled + code cleanup
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- src/main.tsx
+++ src/main.tsx
@@ -18,23 +18,12 @@
  *
  */
 import * as React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/styles/index.css';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 );
